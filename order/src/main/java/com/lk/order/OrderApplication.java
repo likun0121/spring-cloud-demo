943fb10acbf1cb42f76f6dadb1977755f2e99fe1
李坤 <478250793@qq.com>
2021-03-28 18:42:35 +0800

    feign hystrix使用

--- order/src/main/java/com/lk/order/OrderApplication.java
+++ order/src/main/java/com/lk/order/OrderApplication.java
@@ -1,11 +1,9 @@
 package com.lk.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 
 //@SpringBootApplication
@@ -13,6 +11,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 //@EnableCircuitBreaker
 @SpringCloudApplication
 @EnableFeignClients(basePackages = "com.lk.product.client")
+@ComponentScan(basePackages = "com.lk")
 public class OrderApplication {
 
     public static void main(String[] args) {
--- product-client/src/main/java/com/lk/product/client/ProductClient.java
+++ product-client/src/main/java/com/lk/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.lk.product.client;
 import com.lk.product.dto.CartDTO;
 import com.lk.product.dto.ProductInfoDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,7 @@ import java.util.List;
 /**
  * @author LK
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.Fallback.class)
 public interface ProductClient {
 
     @GetMapping("/msg")
@@ -24,4 +25,21 @@ public interface ProductClient {
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<CartDTO> cartDTOList);
 
+    @Component
+    class Fallback implements ProductClient {
+        @Override
+        public String productMsg() {
+            return "异常了";
+        }
+
+        @Override
+        public List<ProductInfoDTO> listByIds(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<CartDTO> cartDTOList) {
+
+        }
+    }
 }
